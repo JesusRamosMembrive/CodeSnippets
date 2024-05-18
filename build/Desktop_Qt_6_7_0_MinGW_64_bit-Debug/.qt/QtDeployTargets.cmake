7eae7f38cf94511dd23008b4476f368f57b53f6c
Jesus Ramos Membrive <jesusramos@overture.life>
2024-05-18 08:33:45 +0200

    Ignore and stop tracking build folder

--- build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appCodeSnippetApp_FILE C:/Users/jesus/Documents/Git/CodeSnippets/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/appCodeSnippetApp.exe)
-set(__QT_DEPLOY_TARGET_appCodeSnippetApp_RUNTIME_DLLS C:/Qt/6.7.0/mingw_64/bin/Qt6Quick.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6QmlModels.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Qml.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Network.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
--- build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/CodeSnippetApp/Modules/ListsOfModels/DataTypesModel.qml
+++
@@ -1,9 +0,0 @@
-import QtQuick
-
-Item {
-    property ListModel dataTypes: ListModel {
-        ListElement { name: "IntegerModifiers" }
-        ListElement { name: "Number system" }
-        ListElement { name: "Varibable initialization" }
-    }
-}
